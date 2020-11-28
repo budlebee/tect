0b9f3f0b8fdc55a9a60a9f7c1875f6aa8fd078fb
budlebee <bbogle7613@gmail.com>
2020-11-28 17:00:04 +0900

    메인 홈화면에서도 폰 가로로 돌리라는 사인 추가

--- pages/index.js
+++ pages/index.js
@@ -212,141 +212,158 @@ export default function App() {
   }, []);
 
   return (
-    <div className="container">
-      <main>
-        <div className="tree">
-          <div ref={containerRef} style={{ height: '600px' }} />
-          <div>
-            contributors
-            <br />
-            budlebeee
+    <>
+      <div className="rotate-alert">
+        <div className="phone"></div>
+        <div className="message">Please rotate your device!</div>
+      </div>
+      <div className="container">
+        <main>
+          <div className="tree">
+            <div ref={containerRef} style={{ height: '600px' }} />
+            <div>
+              contributors
+              <br />
+              budlebeee
+            </div>
           </div>
-        </div>
-      </main>
-      <aside className="sidebar">
-        <Link
-          href="/techtree/physics"
-          as={process.env.BACKEND_URL + '/techtree/physics'}
-        >
-          <a>
-            <div className="block">
-              <img src="../static/icons/physics.svg" height="70" width="70" />
-              <br />
-              Physics
-            </div>
-          </a>
-        </Link>
+        </main>
+        <aside className="sidebar">
+          <Link
+            href="/techtree/physics"
+            as={process.env.BACKEND_URL + '/techtree/physics'}
+          >
+            <a>
+              <div className="block">
+                <img src="../static/icons/physics.svg" height="70" width="70" />
+                <br />
+                Physics
+              </div>
+            </a>
+          </Link>
 
-        <Link
-          href="/techtree/math"
-          as={process.env.BACKEND_URL + '/techtree/math'}
-        >
-          <a>
-            <div className="block">
-              <img src="../static/icons/math.svg" height="70" width="70" />
-              <br />
-              Mathematics
-            </div>
-          </a>
-        </Link>
+          <Link
+            href="/techtree/math"
+            as={process.env.BACKEND_URL + '/techtree/math'}
+          >
+            <a>
+              <div className="block">
+                <img src="../static/icons/math.svg" height="70" width="70" />
+                <br />
+                Mathematics
+              </div>
+            </a>
+          </Link>
 
-        <Link
-          href="/techtree/electricalengineering"
-          as={process.env.BACKEND_URL + '/techtree/electricalengineering'}
-        >
-          <a>
-            <div className="block">
-              <img
-                src="../static/icons/electricity.svg"
-                height="50"
-                width="50"
-              />
-              <br />
-              Electrical
-              <br />
-              Engineering
-            </div>
-          </a>
-        </Link>
+          <Link
+            href="/techtree/electricalengineering"
+            as={process.env.BACKEND_URL + '/techtree/electricalengineering'}
+          >
+            <a>
+              <div className="block">
+                <img
+                  src="../static/icons/electricity.svg"
+                  height="50"
+                  width="50"
+                />
+                <br />
+                Electrical
+                <br />
+                Engineering
+              </div>
+            </a>
+          </Link>
 
-        <Link
-          href="/techtree/economy"
-          as={process.env.BACKEND_URL + '/techtree/economy'}
-        >
-          <a>
-            <div className="block">
-              <img src="../static/icons/economy.svg" height="70" width="70" />
-              <br />
-              Economics
-            </div>
-          </a>
-        </Link>
-        <Link
-          href="/techtree/chemistry"
-          as={process.env.BACKEND_URL + '/techtree/chemistry'}
-        >
-          <a>
-            <div className="block">
-              <img src="../static/icons/chemistry.svg" height="70" width="70" />
-              <br />
-              Chemistry
-            </div>
-          </a>
-        </Link>
-        <Link
-          href="/techtree/biochemistry"
-          as={process.env.BACKEND_URL + '/techtree/biochemistry'}
-        >
-          <a>
-            <div className="block">
-              <img
-                src="../static/icons/biochemistry.svg"
-                height="70"
-                width="70"
-              />
-              <br />
-              Biochemistry
-            </div>
-          </a>
-        </Link>
-        <Link
-          href="/techtree/russia"
-          as={process.env.BACKEND_URL + '/techtree/russia'}
-        >
-          <a>
-            <div className="block">
-              <img src="../static/icons/russia.svg" height="70" width="70" />
-              <br />
-              Russia
-            </div>
-          </a>
-        </Link>
-        <Link href="/techtree/cs" as={process.env.BACKEND_URL + '/techtree/cs'}>
-          <a>
-            <div className="block">
-              <img src="../static/icons/computer.svg" height="50" width="50" />
-              <br />
-              Computer
-              <br />
-              Science
-            </div>
-          </a>
-        </Link>
-        <Link
-          href="/techtree/earthsystem"
-          as={process.env.BACKEND_URL + '/techtree/earthsystem'}
-        >
-          <a>
-            <div className="block">
-              <img src="../static/icons/earth.svg" height="50" width="50" />
-              <br />
-              Earth
-              <br />
-              System
-            </div>
-          </a>
-        </Link>
-      </aside>
-    </div>
+          <Link
+            href="/techtree/economy"
+            as={process.env.BACKEND_URL + '/techtree/economy'}
+          >
+            <a>
+              <div className="block">
+                <img src="../static/icons/economy.svg" height="70" width="70" />
+                <br />
+                Economics
+              </div>
+            </a>
+          </Link>
+          <Link
+            href="/techtree/chemistry"
+            as={process.env.BACKEND_URL + '/techtree/chemistry'}
+          >
+            <a>
+              <div className="block">
+                <img
+                  src="../static/icons/chemistry.svg"
+                  height="70"
+                  width="70"
+                />
+                <br />
+                Chemistry
+              </div>
+            </a>
+          </Link>
+          <Link
+            href="/techtree/biochemistry"
+            as={process.env.BACKEND_URL + '/techtree/biochemistry'}
+          >
+            <a>
+              <div className="block">
+                <img
+                  src="../static/icons/biochemistry.svg"
+                  height="70"
+                  width="70"
+                />
+                <br />
+                Biochemistry
+              </div>
+            </a>
+          </Link>
+          <Link
+            href="/techtree/russia"
+            as={process.env.BACKEND_URL + '/techtree/russia'}
+          >
+            <a>
+              <div className="block">
+                <img src="../static/icons/russia.svg" height="70" width="70" />
+                <br />
+                Russia
+              </div>
+            </a>
+          </Link>
+          <Link
+            href="/techtree/cs"
+            as={process.env.BACKEND_URL + '/techtree/cs'}
+          >
+            <a>
+              <div className="block">
+                <img
+                  src="../static/icons/computer.svg"
+                  height="50"
+                  width="50"
+                />
+                <br />
+                Computer
+                <br />
+                Science
+              </div>
+            </a>
+          </Link>
+          <Link
+            href="/techtree/earthsystem"
+            as={process.env.BACKEND_URL + '/techtree/earthsystem'}
+          >
+            <a>
+              <div className="block">
+                <img src="../static/icons/earth.svg" height="50" width="50" />
+                <br />
+                Earth
+                <br />
+                System
+              </div>
+            </a>
+          </Link>
+        </aside>
+      </div>
+    </>
   );
 }
